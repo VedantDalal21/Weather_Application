c2f735db3c773da31029095a8deb3d26ca58269d
VedantDalal21 <dalalvedant2110@gmail.com>
2026-02-24 10:39:08 +0000

    Day2

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ let w_humidity = document.querySelector(".weather_humidity");
 let w_wind = document.querySelector(".weather.wind");
 let w_pressure = document.querySelector(".weather_pressure");
 
+let citySearch = document.querySelector(".weather_search");
+
 // to get the actual country name 
 // 4
 const getCountryName = (code) =>{
@@ -40,14 +42,30 @@ const getDateTime = (dt) =>{
 
 }
 
+//8 to get city 
+let city = "Mumbai";
+
+//search functionality
+citySearch.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    let cityName = document.querySelector(".city_name");
+    console.log(cityName.value);
+    city= cityName.value;
+
+    getWeatherData();
+
+    cityName.value = "";
+})
+
 // 2
 const getWeatherData = async ()=>{
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=19.07&lon=72.87&appid=17da0c9d91ab9592fee5a927f9497ec5`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=17da0c9d91ab9592fee5a927f9497ec5`;
     try {
         const res = await fetch(weatherUrl);
         const data = await res.json();
         // 3. 
-        // console.log(data)
+        console.log(data)
 
         const {main, name, weather, wind, sys, dt} = data;
 
@@ -57,12 +75,19 @@ const getWeatherData = async ()=>{
         // 5.
         dateTime.innerHTML = getDateTime(dt);
 
+        //7.add city icon
+        w_forcast.innerHTML = weather[0].main;
+        w_icon.innerHTML = `<img src = "http://openweathermap.org/img/wn/${weather[0].icon}@4x.png" />`;
+
         // 6.
         w_temprature.innerHTML = `${main.temp}&#176`;
         w_minTem.innerHTML = `Min: ${main.temp_min.toFixed()}&#176`
         w_maxTem.innerHTML = `Max: ${main.temp_max.toFixed()}&#176`
 
-        w_feelsLike = `${main.feels_like}&#176`
+        w_feelsLike.innerHTML = `${main.feels_like.toFixed(2)}&#176`;
+        w_humidity.innerHTML = `${main.humidity}%`;
+        w_wind.innerHTML = `${wind.speed} m/s`;
+        w_pressure.innerHTML = `${main.pressure} hPa`;
     } catch (error) {
         console.log(error);
     }
